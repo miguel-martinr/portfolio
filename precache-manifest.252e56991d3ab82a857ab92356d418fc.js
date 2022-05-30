5a91801bd338eec8bfe5ed635e89131e086d1fca
Miguel Martín <alu0101209777@ull.edu.es>
2022-05-31 00:12:58 +0100

    Updates

---
+++ precache-manifest.252e56991d3ab82a857ab92356d418fc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "417c14dd60215d885d1ddc0de9902e94",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "49d6ee43883a2916bf7e",
+    "url": "/portfolio/static/css/main.af6d9b4f.chunk.css"
+  },
+  {
+    "revision": "64e875f64c045cb724de",
+    "url": "/portfolio/static/js/2.f2cd3790.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/portfolio/static/js/2.f2cd3790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49d6ee43883a2916bf7e",
+    "url": "/portfolio/static/js/main.c88c5c87.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/portfolio/static/js/main.c88c5c87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3fe483896071cc46d75",
+    "url": "/portfolio/static/js/runtime-main.75c69042.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/portfolio/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "df1364e69578220d3c8f1c1d7b2f6806",
+    "url": "/portfolio/static/media/logofresh.df1364e6.svg"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/portfolio/static/media/resume.9d3da0e2.pdf"
+  }
+]);
